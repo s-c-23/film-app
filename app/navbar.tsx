99642b9f1d593480f4a08ef16b306d0e4626066e
s-c-23 <shyamli.channa@utexas.edu>
2024-10-29 22:54:20 -0500

    first commit

---
+++ app/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+
+<nav className="navbar">
+  <h1>About Us!</h1>
+  <h1>How to Join</h1>
+  <h1>Login</h1>
+  <h1>Articles</h1>
+  <h1>Forums</h1>
+</nav>
+);
+};
+
+export default Navbar;
